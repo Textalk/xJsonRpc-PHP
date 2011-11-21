fcbeb670743aa2b283ba6f033168dedb1d47c9e2
Xeross <xeross@theelitist.net>
2011-11-21 15:05:06 +0100

    Implement calling remote methods on client

--- client.php
+++ client.php
@@ -24,14 +24,14 @@ class Jsonrpc20WebClient
 
     public function __construct($endpoint)
     {
-        $this->endpoint = $endpoints;
-        $this->notify = Jsonrpc20WebClientNotify($this);
+        $this->endpoint = $endpoint;
+        $this->notify = new Jsonrpc20WebClientNotify($this);
     }
 
     public function __call($method, $args)
     {
         $request = $this->assemble_request($method, $args);
-        return $this->json_encode($request);
+        return $this->send_request($request);
     }
 
     public function notify($method, $args)
@@ -42,7 +42,31 @@ class Jsonrpc20WebClient
 
     protected function assemble_request($method, $args, $notification = false)
     {
+         return array(
+              'method'  => $method,
+              'params'  => $args,
+              'id'      => $notification ? null : 1,
+              'jsonrpc' => '2.0'
+         );
     }
+
+    protected function send_request(array $request)
+    {
+         $request_json = $this->json_encode($request);
+
+         $curl = curl_init($this->endpoint);
+
+         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($curl, CURLOPT_POSTFIELDS, $request_json);
+
+         $result = curl_exec($curl);
+         return $this->parse_result($result);
+     }
+
+     protected function parse_result($result_json)
+     {
+          echo $result_json;
+     }
 
     protected function json_encode($request)
     {
