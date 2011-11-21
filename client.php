8c97a94539d56e8c49bc4684ebe43a375e85aff3
Xeross <xeross@theelitist.net>
2011-11-21 16:05:49 +0100

    Lay some groundwork in client, fix some indenting and add validation

--- client.php
+++ client.php
@@ -45,35 +45,104 @@ class Jsonrpc20WebClient
 
     protected function assemble_request($method, $args, $notification = false)
     {
-         return array(
-              'method'  => $method,
-              'params'  => $args,
-              'id'      => $notification ? null : 1,
-              'jsonrpc' => '2.0'
-         );
+        return array(
+            'method'  => $method,
+            'params'  => $args,
+            'id'      => $notification ? null : 1,
+            'jsonrpc' => '2.0'
+        );
     }
 
     protected function send_request(array $request)
     {
-         $request_json = $this->json_encode($request);
+        $request_json = $this->_encode_json($request);
 
-         $curl = curl_init($this->endpoint);
+        $curl = curl_init($this->endpoint);
 
-         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($curl, CURLOPT_POSTFIELDS, $request_json);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request_json);
 
-         $result = curl_exec($curl);
-         return $this->parse_result($result);
-     }
+        $result = curl_exec($curl);
 
-     protected function parse_result($result_json)
-     {
-          echo $result_json;
-     }
+        return $this->_delegate_response($result);
+    }
 
-    protected function json_encode($request)
+    protected function _handle_success_response($response, $reqid)
     {
-        return json_encode($request);
+        // ...
+    }
+
+    protected function _handle_error_response($response, $reqid)
+    {
+        return $response;
+    }
+
+    protected function _delegate_response($result)
+    {
+        if(is_assoc($result))
+            return $this->_handle_response($result);
+        else
+            return $this->_handle_batch_response($result);
+    }
+
+    protected function _handle_batch_response($responses)
+    {
+        $result = array();
+        foreach($responses as $response)
+            $result[] = $this->_hande_response($response);
+
+        return $result;
+    }
+
+    protected function _handle_response ($response)
+    {
+        try
+        {
+            $this->_validate_response($response);
+        }
+        catch(JsonrpcException $e)
+        {
+            return $this->_handle_error_response($e, NULL);
+        }
+
+        try
+        {
+
+        }
+        catch(JsonrpcException $e)
+        {
+
+        }
+    }
+
+    protected function _validate_response($response)
+    {
+        if(!array_key_exists("id", $response))
+            throw new JsonrpcInvalidRequestError("Missing id");
+
+        if(!array_key_exists("jsonrpc", $response) || $response["jsonrpc"] != "2.0")
+            throw new JsonrpcInvalidVersionError();
+
+        if(!array_key_exists('result', $response) && !array_key_exists('error', $response))
+            throw new JsonrpcInvalidRequestError("No error or result in response");
+
+        if(!is_numeric($response["id"]) && !is_string($response["id"]))
+            throw new JsonrpcInvalidRequestError("id isn't string or int but " . gettype($request["id"]));
+
+        return true;
+    }
+
+    protected function _parse_json($json)
+    {
+        $result = json_decode($json, true);
+        if($result === NULL)
+            throw new JsonrpcParseError();
+
+        return $result;
+    }
+    protected function _encode_json($result)
+    {
+        return json_encode($result);
     }
 }
 ?>
