700090b9162d67bbabd6f8c458d1a570f92fb5fd
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2012-10-04 14:01:13 +0200

    Wrapping function is_assoc in check for function_exists.

--- helpers.php
+++ helpers.php
@@ -1,11 +1,13 @@
 <?php
-function is_assoc(array $array)
-{
-    foreach($array as $key => $value)
+if (!function_exists('is_assoc')) {
+    function is_assoc(array $array)
     {
-        if(!is_numeric($key))
-            return true;
+        foreach($array as $key => $value)
+        {
+            if(!is_numeric($key))
+                return true;
+        }
+        return false;
     }
-    return false;
 }
 ?>
